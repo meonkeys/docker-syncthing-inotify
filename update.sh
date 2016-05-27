f8c88ff349a58c290b9970dcabeab240d7e5e8b9
Adam Monsen <haircut@gmail.com>
2016-05-27 11:15:29 -0700

    improve update script
    Builds are now done by Docker Hub.

--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-set -e
+
+set -o errexit
+set -o nounset
+set -o pipefail
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 current="$(git ls-remote --tags https://github.com/syncthing/syncthing-inotify.git | grep -v '\^{}$' | grep -vE 'beta|rc' | cut -d/ -f3 | sort -V | tail -1)"
 current="${current#v}"
 
-set -x
 sed -ri 's/^(ENV SYNCTHING_INOTIFY_VERSION) .*/\1 '"$current"'/' Dockerfile
-docker build -t meonkeys/syncthing-inotify .
-docker tag meonkeys/syncthing-inotify:latest "meonkeys/syncthing-inotify:v$current"
-docker push meonkeys/syncthing-inotify:latest
-docker push "meonkeys/syncthing-inotify:v$current"
+git commit -am "update syncthing-inotify to latest (v$current)"
+git tag "v$current"
