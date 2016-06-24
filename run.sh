3092c95ab6be3f1e6a527cae6f05b8748db26104
Adam Monsen <haircut@gmail.com>
2016-06-24 10:46:34 -0700

    swap out tabs for spaces

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 set -e
 
 if [ "$HOME" = '/home/user' ]; then
-	echo >&2 'uh oh, HOME=/home/user'
-	exit 1
+  echo >&2 'uh oh, HOME=/home/user'
+  exit 1
 fi
 
 mkdir -p "$HOME/.config/syncthing"
@@ -11,15 +11,15 @@ mkdir -p "$HOME/.config/syncthing"
 SYNCTHING_INOTIFY_VERSION=0.8.2
 
 set -x
-#	-v /etc:/host/etc \
+# -v /etc:/host/etc \
 docker run -d \
-	--name syncthing-inotify \
-	--restart always \
-	-u "$(id -u):$(id -g)" \
-	-v "$HOME:$HOME" \
-	-v "$HOME/.config/syncthing:/home/user/.config/syncthing" \
-	-v /etc:/etc \
-	-v /mnt:/mnt \
-	--net host \
-	"meonkeys/syncthing-inotify:$SYNCTHING_INOTIFY_VERSION" "$@"
+  --name syncthing-inotify \
+  --restart always \
+  -u "$(id -u):$(id -g)" \
+  -v "$HOME:$HOME" \
+  -v "$HOME/.config/syncthing:/home/user/.config/syncthing" \
+  -v /etc:/etc \
+  -v /mnt:/mnt \
+  --net host \
+  "meonkeys/syncthing-inotify:$SYNCTHING_INOTIFY_VERSION" "$@"
 timeout 10s docker logs -f syncthing-inotify || true
